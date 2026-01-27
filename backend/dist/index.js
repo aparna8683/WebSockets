8a5126cc252c3f487c35145e27a25c0c2a53d1df
Aparna Singh <aparnasingh868383@gmail.com>
2026-01-27 22:26:55 +0530

    WebSocket Main Server

---
+++ backend/dist/index.js
@@ -0,0 +1,20 @@
+import { WebSocketServer, WebSocket } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+// reference to socket is socket similar to req and res
+let userCount = 0;
+let allSocket = [];
+wss.on("connection", (socket) => {
+    userCount++;
+    allSocket.push(socket);
+    console.log("user connected " + " " + userCount);
+    socket.on("message", (event) => {
+        console.log("message received" + event.toString() + " from user " + userCount);
+        for (const s of allSocket) {
+            s.send(event.toString() + ": sent from the server this message is from the user" + userCount);
+        }
+        // setInterval(()=>{
+        //     socket.send(event.toString()+": sent from the server this message is from the user"+ userCount)
+        // },1000)
+    });
+});
+//# sourceMappingURL=index.js.map
