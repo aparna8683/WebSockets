3685a2e93fbfac505d540edf44f077c675e5a6f6
Aparna Singh <aparnasingh868383@gmail.com>
2026-01-28 09:34:26 +0530

    Core logic for broadcasting

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,20 +1,45 @@
 import { WebSocketServer, WebSocket } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
-// reference to socket is socket similar to req and res
-let userCount = 0;
 let allSocket = [];
 wss.on("connection", (socket) => {
-    userCount++;
-    allSocket.push(socket);
-    console.log("user connected " + " " + userCount);
-    socket.on("message", (event) => {
-        console.log("message received" + event.toString() + " from user " + userCount);
-        for (const s of allSocket) {
-            s.send(event.toString() + ": sent from the server this message is from the user" + userCount);
+    //@ts-ignore
+    socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            allSocket.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
         }
-        // setInterval(()=>{
-        //     socket.send(event.toString()+": sent from the server this message is from the user"+ userCount)
-        // },1000)
+        if (parsedMessage.type === "chat") {
+            // const currentUserRoom= allSocket.find((x)=>
+            // x.socket==socket).room
+            let currentUserRoom = null;
+            for (const user of allSocket) {
+                if (user.socket === socket) {
+                    currentUserRoom = user.room;
+                    break;
+                }
+            }
+            for (const user of allSocket) {
+                if (user.room === currentUserRoom)
+                    user.socket.send(parsedMessage.payload.message);
+            }
+        }
     });
 });
+// wss.on("connection",(socket)=>{
+//         userCount++;
+//         allSocket.push(socket)
+//     console.log("user connected " +" " + userCount)
+//     socket.on("message",(event)=>{
+//         console.log("message received"+ event.toString()+" from user "+ userCount)
+// for (const s of allSocket) {
+//             s.send(event.toString()+": sent from the server this message is from the user"+ userCount)
+//         }
+//         // setInterval(()=>{
+//         //     socket.send(event.toString()+": sent from the server this message is from the user"+ userCount)
+//         // },1000)
+//     })
+// })
 //# sourceMappingURL=index.js.map
